f5fe34990c2f86cd1f41f2fa389dc88006d6f67a
keyatyagi <77800952+keyatyagi@users.noreply.github.com>
2023-01-28 15:38:11 -0500

    Add files via upload
    pomodoro timer script!

---
+++ TimerScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+
+    public float  timeValue = 1500;
+    public Text timeText;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+        DisplayTime(timeValue);        
+
+    }
+
+    // function to display the time on screen in minutes and seconds!
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        //if less than 0, make it 0 to avoid glitch!
+        {
+            timeToDisplay = 0;
+        }
+        
+        // minutes : seconds/60, rounded down
+        float minutes = Mathf.FloorToInt(timeToDisplay/60);
+        // seconds : use modulo to get remainder after dividing by 60 
+        float seconds = Mathf.FloorToInt(timeToDisplay%60);
+
+
+        // now to display minutes and seconds : 
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+
+
+    }
+}
